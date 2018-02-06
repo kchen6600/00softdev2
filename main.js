31db04b9a9e3e73eaa2c61f946d1e7190b484423
Karen Chen <kchen6600@gmail.com>
2018-02-05 19:59:22 -0500

    updated

--- main.js
+++ main.js
@@ -4,6 +4,10 @@ var clearb = document.getElementById("clear");
 var toggleb = document.getElementById("toggle");
 
 var circle = true;
+var shapeDrawn = false;
+
+var oldX = 0;
+var oldY = 0;
 
 var tog = function() {
     if (circle) {
@@ -17,21 +21,48 @@ var tog = function() {
 toggleb.addEventListener('click', tog, true);
 
 var draw = function(e) {
-    var x = e.clientX;
-    var y = e.clientY;
-    var dim = 50;
-    ctx.beginPath();
-    if (circle) {
-        ctx.arc(x - 5, y - 5, dim, 0, 2 * Math.PI);
-        ctx.stokeStyle = "#800000";
-        ctx.stroke();
-		ctx.fillStyle = "#800000";
-        ctx.fill();
-    } else {
-        ctx.fillStyle = "#808000";
-        ctx.fillRect(x - dim, y - dim, dim*2, dim*2);
-    }
+    var x = e.offsetX;
+    var y = e.offsetY;
+	ctx.beginPath();
+	
+	if (shapeDrawn) {
+		ctx.lineTo(oldX, oldY);
+		ctx.stroke();
+		var dim = 20;
+      	if (circle) {
+			ctx.arc(x - 5, y - 5, dim, 0, 2 * Math.PI);
+           	ctx.stokeStyle = "#800000";
+           	ctx.stroke();
+   		   	ctx.fillStyle = "#800000";
+           	ctx.fill();
+      	 } else {
+			ctx.fillStyle = "#808000";
+			ctx.fillRect(x - dim, y - dim, dim*2, dim*2);
+			ctx.moveTo(x,y);
+			ctx.lineTo(oldX, oldY);
+			ctx.stroke();
+      	 }
+	}
+	else{
+		var dim = 20;
+		if (circle) {
+			ctx.arc(x - 5, y - 5, dim, 0, 2 * Math.PI);
+        	ctx.stokeStyle = "#800000";
+       	 	ctx.stroke();
+			ctx.fillStyle = "#800000";
+       	 	ctx.fill();
+			shapeDrawn = true;
+		} else {
+        	ctx.fillStyle = "#808000";
+        	ctx.fillRect(x - dim, y - dim, dim*2, dim*2);
+			shapeDrawn = true;
+   	 	}
+	}
+	
+	oldX = x;
+	oldY = y;
 };
+
 
 canv.addEventListener('click', draw, true);
 
